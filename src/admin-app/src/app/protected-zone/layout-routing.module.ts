158d7401a2047eaf5d9b6eb5fd903c858fd52cb7
lucdv <lucdv>
2022-01-03 16:19:28 +0700

    add routing for component and module

--- src/admin-app/src/app/protected-zone/layout-routing.module.ts
+++ src/admin-app/src/app/protected-zone/layout-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout.component';
+// import { AuthGuard } from '@app/shared';
 
 const routes: Routes = [
     {
@@ -9,9 +10,34 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
             {
-                path: 'dashboard',
-                loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
-            }
+                path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+                // data: {
+                //     functionCode: 'DASHBOARD'
+                // },
+                // canActivate: [AuthGuard]
+            },
+            {
+                path: 'contents', loadChildren: () => import('./contents/contents.module').then(m => m.ContentsModule),
+                // data: {
+                //     functionCode: 'CONTENT'
+                // },
+                // canActivate: [AuthGuard]
+            },
+            {
+                path: 'systems', loadChildren: () => import('./systems/systems.module').then(m => m.SystemsModule),
+                // data: {
+                //     functionCode: 'SYSTEM'
+                // },
+                // canActivate: [AuthGuard]
+            },
+            {
+                path: 'statistics', loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule),
+                // data: {
+                //     functionCode: 'STATISTIC'
+                // },
+                // canActivate: [AuthGuard]
+            },
+
         ]
     }
 ];
@@ -20,4 +46,4 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class LayoutRoutingModule {}
+export class LayoutRoutingModule { }
